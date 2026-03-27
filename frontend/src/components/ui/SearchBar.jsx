c277d69632d2f34f7b974c3c850fe222b8c7274c
Inder <117798788+Inderpreetsinghsaini@users.noreply.github.com>
2026-03-27 16:57:47 -0400

    Update SearchBar.jsx

--- frontend/src/components/ui/SearchBar.jsx
+++ frontend/src/components/ui/SearchBar.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+// import React from "react";
+
+// function SearchBar({ searchTerm, setSearchTerm }) {
+//   return (
+//     <input
+//       type="text"
+//       placeholder="Search by name..."
+//       value={searchTerm}
+//       onChange={(e) => setSearchTerm(e.target.value)}
+//       style={{
+//         padding: "0.5rem",
+//         width: "100%",
+//         marginBottom: "1rem"
+//       }}
+//     />
+//   );
+// }
+
+// export default SearchBar;
+
+
+// earlier code above ^^^^^^^^^^^
+//   NEW CODE BELOW
+
+
+
+import React from 'react';
 
 function SearchBar({ searchTerm, setSearchTerm }) {
   return (
-    <input
-      type="text"
-      placeholder="Search by name..."
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
-      style={{
-        padding: "0.5rem",
-        width: "100%",
-        marginBottom: "1rem"
-      }}
-    />
+    <div className="search-bar-container">
+      <input 
+        type="text" 
+        placeholder="Search by model, make, or year..." 
+        className="inventory-search" 
+        value={searchTerm} 
+        onChange={(e) => setSearchTerm(e.target.value)} 
+      />
+    </div>
   );
 }
 
